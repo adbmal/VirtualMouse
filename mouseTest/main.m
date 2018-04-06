81ccebd5cec0982a6cf0e669823f62e1af186b1b
Yiwei Wang <1wei@live.com>
2018-04-06 19:57:53 +0800

    Initial Commit

---
+++ mouseTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mouseTest
+//
+//  Created by Yiwei Wang on 06/04/2018.
+//  Copyright © 2018 Yiwei Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
